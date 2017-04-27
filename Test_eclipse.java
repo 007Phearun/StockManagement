c49abb99394351195a04ee9b9ac511111c0e8aa2
007Phearun <phearunkh@hotmail.com>
2017-04-27 16:33:00 +0700

    Create Test_eclipse.java

---
+++ Test_eclipse.java
@@ -0,0 +1,14 @@
+public class Test_eclipse {
+
+	/**
+	 * @param args
+	 */
+	public Test_eclispe(){
+		System.out.println("tested");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Test_eclipse();
+	}
+
+}
